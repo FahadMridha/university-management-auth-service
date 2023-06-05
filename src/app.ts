a7113abab525e0d93231ac358206798e656fee5e
fahad Mridha <sohelcse16@gmail.com>
2023-06-05 14:07:14 +0600

    [DONE] create user service

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
-app.use(cors)
+app.use(cors())
 
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+//Application  routes
+app.use('/api/v1/users/', usersRouter)
 
 //Testing
 app.get('/', (req: Request, res: Response) => {
